689b98c2a9bfcaa8b46788fa3c13ccbdc7a6148f
Joppe Boerop <j.w.boerop@student.tudelft.nl>
2025-05-07 22:09:27 +0200

    Improve formatting

--- convert_poms.py
+++ convert_poms.py
@@ -2,11 +2,13 @@ import ast
 import base64
 
 with open("data/poms.json") as poms:
-    poms_list = ast.literal_eval(poms.read())
 
-for pom in poms_list:
-    name = list(pom.keys())[0]
-    content = base64.b64decode(pom[name])
+    for pom in poms:
+        if pom == "\n":
+            continue
+        pom_list = ast.literal_eval(pom)
+        name = pom_list[0]
+        content = base64.b64decode(pom_list[1])
 
-    with open(f"data/poms/{name.replace('/', '+')}.xml", mode="wb+") as file:
-        file.write(content)
+        with open(f"data/poms/{name.replace('/', '+')}.xml", mode="wb+") as file:
+            file.write(content)
--- filter_maven.py
+++ filter_maven.py
@@ -17,29 +17,30 @@ with open("data/repos.json", encoding="utf-8") as repos_file:
     repos = repos_json["items"]
 
 count = 0
-poms = []
 response_codes = dict()
 
-for repo in repos:
-    name = repo["name"]
-    response = requests.get(f"https://api.github.com/repos/{name}/contents/pom.xml", headers=headers)
+with open("data/poms.json", mode="w+") as poms_file:
+    poms_file.write("")
 
-    while response.status_code == 429 or response.status_code == 403:
-        print(f"Rate limit exceeded. Repos left: {len(repos) - count}. Trying again at {(datetime.now() + timedelta(seconds=600)).strftime("%H:%M:%S")}")
-        time.sleep(600)
+with open("data/poms.json", mode="a+") as poms_file:
+    for repo in repos:
+        name = repo["name"]
         response = requests.get(f"https://api.github.com/repos/{name}/contents/pom.xml", headers=headers)
 
-    count += 1
-    if response.status_code == 200:
-        response_json = json.loads(response.text)
-        poms.append({name: response_json["content"]})
+        while response.status_code == 429 or response.status_code == 403:
+            print(f"Rate limit exceeded. Repos left: {len(repos) - count}. Trying again at {(datetime.now() + timedelta(seconds=600)).strftime("%H:%M:%S")}")
+            time.sleep(600)
+            response = requests.get(f"https://api.github.com/repos/{name}/contents/pom.xml", headers=headers)
 
-    if count % 50 == 0:
-        print(f"Repo {count} of {len(repos)}")
+        count += 1
+        if response.status_code == 200:
+            response_json = json.loads(response.text)
+            pom_list = [name, response_json["content"]]
+            poms_file.write(f"{pom_list}\n")
 
-    response_codes[response.status_code] = response_codes.get(response.status_code, 0) + 1
+        if count % 50 == 0:
+            print(f"Repo {count} of {len(repos)}")
 
-with open("data/poms.json", mode="w+") as poms_file:
-    poms_file.write(json.dumps(poms))
+        response_codes[response.status_code] = response_codes.get(response.status_code, 0) + 1
 
 print(response_codes)
